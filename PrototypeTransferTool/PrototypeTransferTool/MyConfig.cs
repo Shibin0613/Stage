b31feb29f85fcf9ec138a47cac48c50f6497dfd7
ShibinPan <220617@edu.rocfriesepoort.nl>
2024-02-22 15:04:08 +0100

    georienteerd en geprobeerd om de data's vanuit pdf te halen

--- PrototypeTransferTool/PrototypeTransferTool/MyConfig.cs
+++ PrototypeTransferTool/PrototypeTransferTool/MyConfig.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 using System.Xml;
+using Org.BouncyCastle.Asn1.X509;
 
 namespace PrototypeTransferTool
 {
@@ -53,24 +54,119 @@ namespace PrototypeTransferTool
         public static string GetTextFromPDF
         {
             get
-            {
+            { 
                 StringBuilder text = new StringBuilder();
-                using (PdfReader reader = new PdfReader(@"C:\Users\pansh\Desktop\test.pdf"))
+                StringBuilder order = new StringBuilder();
+                using (PdfReader reader = new PdfReader(@"C:\Users\pansh\Desktop\Stage\TransferTool\test1.pdf"))
                 {
                     for (int i = 1; i <= reader.NumberOfPages; i++)
                     {
+                        //test position voor Order number
+                        System.util.RectangleJ rect = new System.util.RectangleJ(402f, 540f, 75f, 15f);
+                        RenderFilter[] filter = { new RegionTextRenderFilter(rect) };
+                        ITextExtractionStrategy strategyForOrdernumber = new FilteredTextRenderListener(
+                            new LocationTextExtractionStrategy(), filter);
+                        string test = PdfTextExtractor.GetTextFromPage(reader, i, strategyForOrdernumber);
+
+                        //Definieren alle text van PDF
                         ITextExtractionStrategy strategy = new SimpleTextExtractionStrategy();
 
-                        string currentText = PdfTextExtractor.GetTextFromPage(reader, i, strategy).Replace("\n", string.Empty);
-                        int orderFrom = currentText.IndexOf("AANTAL PRIJS EENHEID TOTAAL") + "AANTAL PRIJS EENHEID TOTAAL".Length;
-                        int orderTo = currentText.LastIndexOf("TOTAAL"[1]);
-                        string Order = currentText.Substring(orderFrom, orderTo - orderFrom);
+                        string currentText = PdfTextExtractor.GetTextFromPage(reader, i, strategy);
 
-                        text.Append(currentText); // Voeg de geëxtraheerde tekst toe aan de StringBuilder
+                        if (currentText.Contains("INKOOP ORDER NUMMER "))
+                        {
+                            //Ordernumber
+                            int ordernumberFrom = currentText.IndexOf("INKOOP ORDER NUMMER ") + "INKOOP ORDER NUMMER ".Length;
+                            int ordernumberTo = currentText.LastIndexOf("INKOOP ORDER DATUM EXCLUSIEF");
+                            string orderNumber = currentText.Substring(ordernumberFrom, ordernumberTo - ordernumberFrom).Replace("\n", "");
+
+                            //Inkoopdatum
+                            int inkoopDatumFrom = currentText.IndexOf("el servicio.") + "el servicio.".Length;
+                            int inkoopDatumTo = currentText.IndexOf("INKOOP ORDER NUMMER ");
+                            string inkoopDatum = currentText.Substring(inkoopDatumFrom, inkoopDatumTo - inkoopDatumFrom).Replace("\n", "");
+
+                            //Hotelnaam op position
+                            System.util.RectangleJ rectHotelnaam = new System.util.RectangleJ(524.16f, 538.68f, 254.88f, 6.84f);
+                            RenderFilter[] filterHotelnaam = { new RegionTextRenderFilter(rectHotelnaam) };
+                            ITextExtractionStrategy strategyHotelnaam = new FilteredTextRenderListener(
+                                new LocationTextExtractionStrategy(), filterHotelnaam);
+                            string hotelNaam = PdfTextExtractor.GetTextFromPage(reader, i, strategyHotelnaam);
+
+                            //Aanvrager
+                            System.util.RectangleJ rectAanvrager = new System.util.RectangleJ(255f, 438f, 152f, 78f);
+                            RenderFilter[] filterAanvrager = { new RegionTextRenderFilter(rectAanvrager) };
+                            ITextExtractionStrategy strategyAanvrager = new FilteredTextRenderListener(
+                                new LocationTextExtractionStrategy(), filterAanvrager);
+                            string Aanvrager = PdfTextExtractor.GetTextFromPage(reader, i, strategyAanvrager);
+
+                            
+                            //afleveradres
+                            int afleverAdresFrom = currentText.IndexOf("OPMERKING :  ") + "OPMERKING :  ".Length;
+                            int afleverAdresTo = currentText.LastIndexOf("LIJN I REFERENTIE");
+                            string afleverAdres = currentText.Substring(afleverAdresFrom, afleverAdresTo - afleverAdresFrom);
+
+
+                            if (currentText.Contains("OPMERKING :  "))
+                            {
+                                //Opmerking
+                                int opmerkingFrom = currentText.IndexOf("NEDERLAND") + "NEDERLAND".Length;
+                                int opmerkingTo = currentText.LastIndexOf(hotelNaam);
+                                string Opmerking = currentText.Substring(opmerkingFrom, opmerkingTo - opmerkingFrom);
+                            }
+
+                            //Order
+                            int orderFrom = currentText.IndexOf("AANTAL PRIJS EENHEID TOTAAL");
+                            string page2 = "Página";
+                            if (currentText.Contains(page2))
+                            {
+                                int orderTo = currentText.LastIndexOf(page2);
+                                string Order = currentText.Substring(0, orderTo - 0);
+                                order.Append(Order);
+                            }
+                            else
+                            {
+                                string orderWithText = currentText.Substring(orderFrom + "AANTAL PRIJS EENHEID TOTAAL".Length);
+                                if (orderWithText.Contains("TOTAAL"))
+                                {
+                                    int orderTo = orderWithText.IndexOf("TOTAAL");
+                                    string Order = orderWithText.Substring(0, orderTo - 0);
+                                    order.Append(Order);
+                                }
+                                else
+                                {
+                                    order.Append(orderWithText);
+                                }
+                            }
+                        }
+
+                        if (currentText.Contains("BETAALCONDITIES"))
+                        {
+                            //Betaalcondities
+                            int betaalFrom = currentText.IndexOf("BETAALCONDITIES") + "BETAALCONDITIES".Length;
+                            int betaalTo = currentText.LastIndexOf("FACTUURADRES");
+                            string betaalConditie = currentText.Substring(betaalFrom, betaalTo - betaalFrom);
+
+                            //FactuurAdres
+                            int factuurAdresFrom = currentText.IndexOf("FACTUURADRES") + "FACTUURADRES".Length;
+                            int factuurAdresTo = currentText.LastIndexOf("STUUR FACTUUR AAN");
+                            string factuurAdres = currentText.Substring(factuurAdresFrom, factuurAdresTo - factuurAdresFrom);
+
+                            //Stuur factuur aan
+                            int stuurFactuuraanFrom = currentText.IndexOf("STUUR FACTUUR AAN") + "STUUR FACTUUR AAN".Length;
+                            int stuurFactuuraanTo = currentText.LastIndexOf("LEVERING");
+                            string stuurFactuurAan = currentText.Substring(stuurFactuuraanFrom, stuurFactuuraanTo - stuurFactuuraanFrom);
+
+                            //Levering
+                            int leveringFrom = currentText.IndexOf("LEVERING") + "LEVERING".Length;
+                            int leveringTo = currentText.LastIndexOf("TOTAAL ");
+                            string Levering = currentText.Substring(leveringFrom, leveringTo - leveringFrom);
+                        }
+
+                        text.Append(currentText);
                     }
                 }
 
-                string xmlFilePath = @"C:\Users\pansh\Desktop\output.xml"; // Het pad naar het XML-bestand
+                string xmlFilePath = @"C:\Users\pansh\Desktop\output1.xml"; // Het pad naar het XML-bestand
 
                 // Schrijf de geëxtraheerde tekst naar een XML-bestand
                 using (XmlWriter writer = XmlWriter.Create(xmlFilePath))
