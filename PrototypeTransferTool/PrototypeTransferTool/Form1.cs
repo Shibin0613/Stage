b8c583a2ffe1bcb6626e750f1d32f9237505c1dc
ShibinPan <220617@edu.rocfriesepoort.nl>
2024-02-29 09:09:25 +0100

    het luisteren naar een map

--- PrototypeTransferTool/PrototypeTransferTool/Form1.cs
+++ PrototypeTransferTool/PrototypeTransferTool/Form1.cs
@@ -7,6 +7,9 @@ namespace PrototypeTransferTool
 {
     public partial class Form1 : Form
     {
+        FileSystemWatcher watcher;
+        private System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
+        private bool isWatching;
         public Form1()
         {
             InitializeComponent();
@@ -18,10 +21,34 @@ namespace PrototypeTransferTool
             blazorWebView1.HostPage = "wwwroot\\index.html";
             blazorWebView1.Services = services.BuildServiceProvider();
             blazorWebView1.RootComponents.Add<BlazorTest>("#app");
+            startWatching();
         }
 
         private void blazorWebView1_Click(object sender, EventArgs e)
         {
+
+        }
+
+        private void startWatching()
+        {
+            isWatching = true;
+            timer.Enabled = true;
+            timer.Start();
+            timer.Interval = 500;
+
+            watcher = new FileSystemWatcher();
+            watcher.Path = MyConfig.FilePath;
+            watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.FileName;
+
+            watcher.Created += new FileSystemEventHandler(OnCreated);
+            
+
+            watcher.EnableRaisingEvents = true;
+        }
+
+        private void OnCreated(object sender, FileSystemEventArgs e)
+        {
+
 
         }
     }
--- PrototypeTransferTool/PrototypeTransferTool/obj/Debug/net8.0-windows/PrototypeTransferTool.AssemblyInfo.cs
+++ PrototypeTransferTool/PrototypeTransferTool/obj/Debug/net8.0-windows/PrototypeTransferTool.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrototypeTransferTool")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfb6282c493e42152d7300159617615777acf6af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5864b535d69d9d6a849f283fa9defafdd67bb8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrototypeTransferTool")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrototypeTransferTool")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
