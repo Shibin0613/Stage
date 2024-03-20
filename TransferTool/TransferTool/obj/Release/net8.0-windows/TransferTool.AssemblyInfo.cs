e7443fb51ee15842f6d69e01e7d090d7b5f78c94
ShibinPan <220617@edu.rocfriesepoort.nl>
2024-03-20 16:43:22 +0100

    release naar debug gereset

--- TransferTool/TransferTool/obj/Release/net8.0-windows/TransferTool.AssemblyInfo.cs
+++ TransferTool/TransferTool/obj/Release/net8.0-windows/TransferTool.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TransferTool")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e5f47371c3314fb26c6ede19e8b58c641e1d239")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ec672f427ef766d954f0a9b09ef634056b91ac8")]
 [assembly: System.Reflection.AssemblyProductAttribute("TransferTool")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TransferTool")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
